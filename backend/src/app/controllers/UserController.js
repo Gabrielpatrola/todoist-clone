c49ddcd9402b35fde038390c7d4ba510ac453596
Gabriel Augusto Almeida <gabriel@patrola.tech>
2022-06-06 23:46:13 -0400

    feat: Adjust controllers

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -69,7 +69,7 @@ class UserController {
 
     await user.update(req.body);
 
-    const { id, name, avatar } = await User.findByPk(req.userId);
+    const { id, name } = await User.findByPk(req.userId);
 
     return res.json({
       id,
