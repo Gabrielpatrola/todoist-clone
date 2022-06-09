cfe419b18fd88c05731005f9c32f1d8d35fd724a
Gabriel Augusto Almeida <gabriel@patrola.tech>
2022-06-08 20:02:57 -0400

    feat: Add heroku configs

--- api/src/server.js
+++ api/src/server.js
@@ -1,3 +1,10 @@
 import app from './app';
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
 
 app.listen(3333);
